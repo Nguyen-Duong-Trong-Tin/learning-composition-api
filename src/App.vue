2f7af06e84dd875c7da4261638492006125849cd
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2026-04-30 22:36:07 +0700

    computed

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
 
   <div class="container">
     <learning-reactivity></learning-reactivity>
-
-    <div class="my-3"></div>
+    <div class="mb-3"></div>
 
     <learning-prop imageUrl="https://images.pexels.com/photos/19884401/pexels-photo-19884401.jpeg"></learning-prop>
+    <div class="mb-3"></div>
 
     <learning-method></learning-method>
+    <div class="mb-3"></div>
+
+    <learning-computed></learning-computed>
   </div>
 </template>
 
@@ -16,13 +19,15 @@
 import LearningReactivity from './components/LearningReactivity.vue';
 import LearningProp from './components/LearningProps.vue';
 import LearningMethod from './components/LearningMethod.vue';
+import LearningComputed from './components/LearningComputed.vue';
 
 export default {
   name: 'App',
   components: {
     LearningReactivity,
     LearningProp,
-    LearningMethod
+    LearningMethod,
+    LearningComputed
   }
 }
 </script>
---
+++ src/components/LearningComputed.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card text-left">
+    <div class="card-body">
+      <h4 class="card-title">Computed</h4>
+
+      <div class="form-group">
+        <label>First name</label>
+        <input type="text" class="form-control" @input="handleChangeFirstName">
+      </div>
+      <div class="form-group">
+        <label>Last name</label>
+        <input type="text" class="form-control" @input="handleChangeLastName">
+      </div>
+
+      <p class="card-text">Fullname: {{ fullName }}</p>
+
+    </div>
+  </div>
+</template>
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+  setup() {
+    const firstName = ref("");
+    const lastName = ref("");
+
+    function handleChangeFirstName(event) {
+      firstName.value = event.target.value;
+    }
+    function handleChangeLastName(event) {
+      lastName.value = event.target.value;
+    }
+
+    const fullName = computed(() => `${firstName.value} ${lastName.value}`);
+
+    return {
+      handleChangeFirstName,
+      handleChangeLastName,
+      fullName
+    }
+  }
+}
+</script>
+<style></style>
